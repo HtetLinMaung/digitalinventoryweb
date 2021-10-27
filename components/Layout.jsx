78908e342033bf77a5af47cdbc0939cde2f85748
hlm <hlm@mit.com.mm>
2021-10-27 10:10:18 +0630

    update

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -291,7 +291,7 @@ export default function Layout({ children }) {
                 role="img"
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 448 512"
-                className="svg-inline--fa fa-bell fa-w-14 fa-3x"
+                className="svg-inline--fa fa-bell fa-w-14 fa-3x d-none d-sm-block"
               >
                 <path
                   fill="currentColor"
@@ -301,37 +301,101 @@ export default function Layout({ children }) {
               </svg>
 
               {state.profile ? (
-                <img src={state.profile} alt="profile" className="avatar" />
+                <div className="dropdown">
+                  <a
+                    className="top__nav-link dropdown-toggle"
+                    href="#"
+                    role="button"
+                    id="dropdownMenuLink"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    <img src={state.profile} alt="profile" className="avatar" />
+                  </a>
+
+                  <ul
+                    className="dropdown-menu"
+                    aria-labelledby="dropdownMenuLink"
+                  >
+                    <li>
+                      <a className="dropdown-item" onClick={handleSignOut}>
+                        Sign Out
+                      </a>
+                    </li>
+                    {/* <li>
+                     <a className="dropdown-item" href="#">
+                       Another action
+                     </a>
+                   </li>
+                   <li>
+                     <a className="dropdown-item" href="#">
+                       Something else here
+                     </a>
+                   </li> */}
+                  </ul>
+                </div>
               ) : (
-                <svg
-                  style={{ width: "2.5rem", margin: 0 }}
-                  aria-hidden="true"
-                  focusable="false"
-                  data-prefix="fad"
-                  data-icon="user-circle"
-                  role="img"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 496 512"
-                  className="svg-inline--fa fa-user-circle fa-w-16 fa-3x"
-                >
-                  <g className="fa-group">
-                    <path
-                      style={{ color: "#9ad6df" }}
-                      fill="currentColor"
-                      d="M248,8C111,8,0,119,0,256S111,504,248,504,496,393,496,256,385,8,248,8Zm0,96a88,88,0,1,1-88,88A88,88,0,0,1,248,104Zm0,344a191.61,191.61,0,0,1-146.5-68.2C120.3,344.4,157.1,320,200,320a24.76,24.76,0,0,1,7.1,1.1,124.67,124.67,0,0,0,81.8,0A24.76,24.76,0,0,1,296,320c42.9,0,79.7,24.4,98.5,59.8A191.61,191.61,0,0,1,248,448Z"
-                    ></path>
-                    <path
-                      style={{ color: "#3aaabf" }}
-                      fill="currentColor"
-                      d="M248,280a88,88,0,1,0-88-88A88,88,0,0,0,248,280Zm48,40a24.76,24.76,0,0,0-7.1,1.1,124.67,124.67,0,0,1-81.8,0A24.76,24.76,0,0,0,200,320c-42.9,0-79.7,24.4-98.5,59.8,68.07,80.91,188.84,91.32,269.75,23.25A192,192,0,0,0,394.5,379.8C375.7,344.4,338.9,320,296,320Z"
-                    ></path>
-                  </g>
-                </svg>
+                <div className="dropdown">
+                  <a
+                    className="top__nav-link dropdown-toggle"
+                    href="#"
+                    role="button"
+                    id="dropdownMenuLink"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    <svg
+                      style={{ width: "2.5rem", margin: 0 }}
+                      aria-hidden="true"
+                      focusable="false"
+                      data-prefix="fad"
+                      data-icon="user-circle"
+                      role="img"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 496 512"
+                      className="svg-inline--fa fa-user-circle fa-w-16 fa-3x"
+                    >
+                      <g className="fa-group">
+                        <path
+                          style={{ color: "#9ad6df" }}
+                          fill="currentColor"
+                          d="M248,8C111,8,0,119,0,256S111,504,248,504,496,393,496,256,385,8,248,8Zm0,96a88,88,0,1,1-88,88A88,88,0,0,1,248,104Zm0,344a191.61,191.61,0,0,1-146.5-68.2C120.3,344.4,157.1,320,200,320a24.76,24.76,0,0,1,7.1,1.1,124.67,124.67,0,0,0,81.8,0A24.76,24.76,0,0,1,296,320c42.9,0,79.7,24.4,98.5,59.8A191.61,191.61,0,0,1,248,448Z"
+                        ></path>
+                        <path
+                          style={{ color: "#3aaabf" }}
+                          fill="currentColor"
+                          d="M248,280a88,88,0,1,0-88-88A88,88,0,0,0,248,280Zm48,40a24.76,24.76,0,0,0-7.1,1.1,124.67,124.67,0,0,1-81.8,0A24.76,24.76,0,0,0,200,320c-42.9,0-79.7,24.4-98.5,59.8,68.07,80.91,188.84,91.32,269.75,23.25A192,192,0,0,0,394.5,379.8C375.7,344.4,338.9,320,296,320Z"
+                        ></path>
+                      </g>
+                    </svg>
+                  </a>
+
+                  <ul
+                    className="dropdown-menu"
+                    aria-labelledby="dropdownMenuLink"
+                  >
+                    <li>
+                      <a className="dropdown-item" onClick={handleSignOut}>
+                        Sign Out
+                      </a>
+                    </li>
+                    {/* <li>
+                    <a className="dropdown-item" href="#">
+                      Another action
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Something else here
+                    </a>
+                  </li> */}
+                  </ul>
+                </div>
               )}
 
               <div className="dropdown">
                 <a
-                  className="top__nav-link dropdown-toggle"
+                  className="top__nav-link dropdown-toggle d-none d-sm-block"
                   href="#"
                   role="button"
                   id="dropdownMenuLink"
